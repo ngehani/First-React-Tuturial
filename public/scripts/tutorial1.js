012c584569d9815d41d13262724c1b9d16652b79
Neil Gehani <njgonline@me.com>
2015-12-23 11:43:32 -0800

    First React Tutorial

---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,37 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  };
+
+});
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="CommentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
